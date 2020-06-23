debcf06c983e02fee1229f80fb9a536c5a48484a
kurogi.kazuki <k-kurogi@winas.jp>
2020-06-23 00:36:37 +0000

    a

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -45,7 +45,7 @@
                     <div class="form-group row">
                         <label class="col-md-2" for="introduction">自己紹介欄</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="introduction" rows="20" value="{{ $profile_form->introduction }}"></textarea></textarea>
+                            <textarea class="form-control" name="introduction" rows="20" value="{{ $profile_form->introduction }}"></textarea>
                         </div>
                     </div>
                     <div class="form-group row">
