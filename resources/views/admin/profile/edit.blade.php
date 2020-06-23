d13bb58f9f85f5dba12773e4528bfa30682eb0cf
kurogi.kazuki <k-kurogi@winas.jp>
2020-06-23 08:41:19 +0000

    resources/views/admin/profile/edit.blade.phpの28行目

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -24,11 +24,12 @@
                         <label class="col-md-2" for="gender">性別</label>
                         <div class="col-md-10">
                             <select name="gender" id="gender">
+                                <input type="text" class="form-control" name="gender" value="{{ $profile_form->gender }}">
                                 <option disabled selected value>選択して下さい</option>
                                 @foreach (["男性","女性","その他"] as $key => $val)
                                     <option
                                         class="form-control"
-                                        <input type="text" class="form-control" name="gender" value="{{ $profile_form->gender }}"
+                                        value="{{ $key }}"
                                     >
                                         {{ $val }}
                                     </option>
