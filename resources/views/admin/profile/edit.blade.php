bdf3f5ba8f8b20bc8617bce1fb35a7045cd314a6
kurogi.kazuki <k-kurogi@winas.jp>
2020-06-23 06:36:59 +0000

    resources/views/admin/profile/edit.blade.phpの31行目

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -28,7 +28,7 @@
                                 @foreach (["男性","女性","その他"] as $key => $val)
                                     <option
                                         class="form-control"
-                                        value="{{ <1key->profile_form->gender>}}"
+                                        value="{{ <$key }}"
                                     >
                                         {{ $val }}
                                     </option>
